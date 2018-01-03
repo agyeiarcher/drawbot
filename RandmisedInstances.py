b25e24c7c78b7c2fe6475fd7de92a30355d0caf5
agyeiarcher <34943654+agyeiarcher@users.noreply.github.com>
2018-01-03 19:05:50 -0400

    Update RandmisedInstances.py

--- RandmisedInstances.py
+++ RandmisedInstances.py
@@ -10,18 +10,19 @@ fontName = installFont(path)
 x, y, w, h = 0, 0, CARDWIDTH, CARDHEIGHT
 
 fill(1)
-rect(0, 0, 249.12, 167.04)
 
-font("crispy")
+font("fontName")
 fill(0)
 
 txtFontSize=106
 
 txt = FormattedString(fontSize=txtFontSize)
 
-txt.fontVariations(wdth=266)
+#set text width
+txt.fontVariations(wdth=266) 
 
-txt.align ="center"
+#'manually' refresh startvariable value each time and append to textBox
+
 startvariable=randint(0,1000)
 txt.fontVariations(wght=startvariable)
 txt.append("C", font="crispy", fontSize=txtFontSize, fill=(0, 0, 0))
@@ -43,7 +44,5 @@ txt.append("E", font="crispy", fontSize=txtFontSize, fill=(0, 0, 0))
 startvariable=randint(0,1000)
 txt.fontVariations(wght=startvariable)
 txt.append("S", font="crispy", fontSize=txtFontSize, fill=(0, 0, 0))
-
-fill(0)
 
 text(txt, (CARDWIDTH/2, CARDHEIGHT/2-txtFontSize/3), align="center")
