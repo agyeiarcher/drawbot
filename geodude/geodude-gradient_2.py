cd464b7528a6944161f95b4c3c3d699723aaa793
Agyei <hello@agyei.design>
2018-08-21 15:18:56 -0400

    fixed for non-valid hex values (i think)

--- geodude/geodude-gradient_2.py
+++ geodude/geodude-gradient_2.py
@@ -7,27 +7,28 @@ sys.path.append("/modules")
 g=geocoder.ip('me')
 
 color1=str(abs(-5.897699))
-
-count=0
-while char!=".":
-    count+=1
-print(count)
-
+color1=format(float(color1))
 print(color1)
-color1=format(float(color1), ".5f")
-str(color1)
+if len(str(color1))<=8:
+    color1=format(float(color1), ".6f")
+else:
+    color1=format(float(color1), ".5f")
+color=str(color1)
 color1=color1.replace(".","")
 color1="#"+(hex(abs(int(color1)))[2:])
 color1=normalize_hex(color1)
 
-color2=22.418589
+color2=str(abs(-5.897699))
+color2=format(float(color2))
 print(color2)
-color2=format(float(color2), ".5f")
-str(color2)
+if len(str(color2))<=8:
+    color2=format(float(color2), ".6f")
+else:
+    color2=format(float(color2), ".5f")
+color=str(color2)
 color2=color2.replace(".","")
-color2="#"+(hex(abs(int(color2)))[2:])
-
-print(color1, color2)
+color1="#"+(hex(abs(int(color2)))[2:])
+color1=normalize_hex(color2)
 
 colorTuple1=hex_to_rgb(color1)
 colorTuple2=hex_to_rgb(color2)
