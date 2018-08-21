5a176569adba1b408a19d8980f0af8a42db33c31
Agyei <hello@agyei.design>
2018-08-21 16:07:27 -0400

    enabled saveImage

--- geodude/geodude-gradient_2.py
+++ geodude/geodude-gradient_2.py
@@ -2,33 +2,33 @@ import sys
 import geocoder
 from webcolors import *
 
-sys.path.append("/modules")
+sys.path.append("/")
 
 g=geocoder.ip('me')
 
-color1=str(abs(-5.897699))
+color1=str(abs(g.lat))
 color1=format(float(color1))
-print(color1)
 if len(str(color1))<=8:
     color1=format(float(color1), ".6f")
-else:
+if len(str(color1))>8:
     color1=format(float(color1), ".5f")
 color=str(color1)
 color1=color1.replace(".","")
 color1="#"+(hex(abs(int(color1)))[2:])
 color1=normalize_hex(color1)
 
-color2=str(abs(-5.897699))
+color2=str(abs(g.lng))
 color2=format(float(color2))
-print(color2)
 if len(str(color2))<=8:
     color2=format(float(color2), ".6f")
-else:
+if len(str(color2))>8:
     color2=format(float(color2), ".5f")
 color=str(color2)
 color2=color2.replace(".","")
-color1="#"+(hex(abs(int(color2)))[2:])
-color1=normalize_hex(color2)
+color2="#"+(hex(abs(int(color2)))[2:])
+color2=normalize_hex(color2)
+
+print(color1, color2)
 
 colorTuple1=hex_to_rgb(color1)
 colorTuple2=hex_to_rgb(color2)
@@ -48,4 +48,4 @@ linearGradient(
 # draw a rectangle
 rect(0, 0, 1000, 1000)
 
-# saveImage("geodude.png")
+saveImage("geodude.png")
